05ca4c72994fa645972f20b7c40da70450761ef3
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2023-11-24 16:04:37 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>Test final + 2 Sync</h2>
+      <h2>Test final + CI/CD</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
